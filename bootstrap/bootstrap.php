179a3b6a60f59fc5c4420b23f5e95fe1fad7eee4
j_polak <jan.polak@onmyway.cz>
2017-10-10 12:07:40 +0200

    controllers & routing

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -2,7 +2,9 @@
 
 // define the constants for directories
 define('SYSTEM_DIR', __DIR__ . '/..'); // system directory
-define('PUBLIC_DIR', SYSTEM_DIR . '/public'); // public directory
+define('APP_DIR', SYSTEM_DIR . '/app'); // application directory
+define('PUBLIC_DIR', SYSTEM_DIR . '/public'); // public director
+define('ROUTES_DIR', SYSTEM_DIR . '/routes'); // route definitions directoryy
 define('VENDOR_DIR', SYSTEM_DIR . '/vendor'); // vendor directory
 
 // require_once all necessary libraries
