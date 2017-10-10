120a07899af6d1080b12c377aaf349569ba6655c
j_polak <jan.polak@onmyway.cz>
2017-10-10 10:55:29 +0200

    bootstrapping, request, db, config, helpers

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
-<?php echo 'Hello, world!'; ?>
+<?php
+
+// include bootstrapping
+include '../bootstrap/bootstrap.php';
+
+var_dump(PUBLIC_DIR);
+
+$route = request('route', 404);
+
+echo $route;
---
+++ vendor/codingbootcamp/tinymvc/helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * alias for \codingbootcamp\tinymvc\request::get
+ */
+function request($name, $default = null)
+{
+    return \codingbootcamp\tinymvc\request::get($name, $default);
+}
